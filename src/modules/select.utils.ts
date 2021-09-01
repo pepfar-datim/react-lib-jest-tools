ec6e903117a2c5575da2b50f1946d12bd9a69523
Jakub <jflaska@baosystems.com>
2021-09-01 14:22:36 -0400

    within

--- src/modules/select.utils.ts
+++ src/modules/select.utils.ts
@@ -1,4 +1,6 @@
-import {fireEvent, screen} from "@testing-library/react";
+import {fireEvent, getByText, screen, within} from "@testing-library/react";
+import {debug} from "./render.utils";
+
 
 /**
  * Select `value` in select element found by `id`
@@ -11,8 +13,17 @@ import {fireEvent, screen} from "@testing-library/react";
  * */
 export function select(id:string, value:string){
     fireEvent.mouseDown(screen.getByTestId(id).childNodes[0]);
-    fireEvent.click(screen.getByText(value));
+    // let container = document.querySelector('[role="presentation"]') as HTMLElement;
+    let modal = screen.getByRole('presentation');
+    // if (value==='Oct - Dec 2020') debug();
+    // console.log('it',container.innerText)
+    // fireEvent.click(getByText(container,value));
+    fireEvent.click(within(modal).getByText(value));
+    // @ts-ignore
+    // delete container;
+    // fireEvent.click(screen.getByText(value));
     checkSelectValue(id, value);
+    debug();
 }
 
 export function selectById(id:string, value:string){
