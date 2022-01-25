368db2e90983d2479403ceda15c57220ce8a862b
Jakub Flaska <jflaska@baosystems.com>
2022-01-25 13:39:08 -0500

    Automatic lib publish

--- src/modules/automatedTest.utils.tsx
+++ src/modules/automatedTest.utils.tsx
@@ -1,7 +1,7 @@
 import {ReactElement} from "react";
-import {testAs} from "@pepfar-react-lib/http-tools";
+import {registerSendMock, testAs} from "@pepfar-react-lib/http-tools";
 import {renderComponent} from "./render.utils";
-import {click, clickByCss} from "./click.utils";
+import {click, clickByCss, clickByText} from "./click.utils";
 import {noTexts, textsWait} from "./text.utils";
 
 
@@ -9,9 +9,14 @@ import {noTexts, textsWait} from "./text.utils";
 export type TestAction = {
     click?: string;
     clickByCss?:string;
+    clickByText?:string;
     texts?: string[];
-    noTexts?: string[]
-    custom?: ()=>void
+    noTexts?: string[];
+    custom?: ()=>void;
+    expectPost?:{
+        url: string;
+        body:any;
+    }
 }
 
 export type TestCase = {
@@ -19,22 +24,29 @@ export type TestCase = {
     component: ReactElement;
     asUser: string;
     actions: TestAction[];
-    setup: ()=>Promise<void>
+    preRender?: ()=>void;
+    postRender?: ()=>Promise<void>;
 }
 
 async function performTestAction(action:TestAction){
     if (action.click) click(action.click);
     if (action.clickByCss) clickByCss(action.clickByCss);
+    if (action.clickByText) clickByText(action.clickByText);
     if (action.texts) await textsWait(action.texts);
     if (action.noTexts) await noTexts(action.noTexts);
     if (action.custom) action.custom();
+    if (action.expectPost) {
+        let response = await registerSendMock('POST',action.expectPost.url, {ok:true})
+        expect(response).toStrictEqual(action.expectPost.body);
+    }
 }
 
-export async function automatedTest({name,component,asUser,actions, setup}:TestCase):Promise<void>{
+export async function automatedTest({name,component,asUser,actions, preRender, postRender}:TestCase):Promise<void>{
     test(name, async()=> {
         testAs(asUser);
+        if (preRender) await preRender();
         await renderComponent(component);
-        await setup();
+        if (postRender) await postRender();
         for (const action of actions) {
             await performTestAction(action);
         }
